f90b37fcdd9c5b8771456a6e7c6b5e4103adc8ec
Dinushan Sriskandaraja <107076816+DinushanSriskandaraja@users.noreply.github.com>
2026-01-29 11:43:27 +0530

    Integrate Google Analytics into layout
    Added Google Analytics scripts for tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-3HJ0CQZV81"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3HJ0CQZV81');
+          `}
+        </Script>
       </body>
     </html>
   );
